7302567fcbc83a38492f566016f968a1ef27abb1
Lucas Leite <carvalho1104@hotmail.com>
2021-03-25 02:08:43 -0300

    adicionei umas informações no footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,44 @@ export default function Footer(){
     return(
         <footer>
             <div className = "footer-container">
-                
+                <div className ="footer-container-info">
+                    <div className ="footer-container-info-1">
+                          <div className ="footer-container-info-1-foto">
+                              <img src="assets/foto_eugenia.jpg"/>
+                          </div>
+
+                          <div className ="footer-container-info-1-descricao">
+                              <h1>Eugenia</h1>
+                              <p>Painter, Amsterdam</p>
+                          </div>    
+                    </div>
+
+                    
+                    <div className ="footer-container-info-1">
+                          <div className ="footer-container-info-1-foto">
+                              <img src="assets/foto_rapaz.jpg"/>
+                          </div>
+
+                          <div className ="footer-container-info-1-descricao">
+                              <h1>Eugenia</h1>
+                              <p>Painter, Amsterdam</p>
+                          </div>  
+
+                    </div>
+
+                   <div className ="footer-container-info-1">
+
+                          <div className ="footer-container-info-1-foto">
+                              <img src="assets/foto_moca.jpg"/>
+                          </div>
+
+                          <div className ="footer-container-info-1-descricao">
+                              <h1>Eugenia</h1>
+                              <p>Painter, Amsterdam</p>
+                          </div>  
+
+                   </div>
+                </div>
             </div>
         </footer>
     )
